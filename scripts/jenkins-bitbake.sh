52b1e189b8dad92267281ee3912a4dd940330a0d
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-06-02 00:27:16 +0000

    add wait loop to jenkins build script

--- scripts/jenkins-bitbake.sh
+++ scripts/jenkins-bitbake.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 cd ${HOME}/yocto
-source poky/oe-init-build-env build
+source poky/oe-init-build-env build-$1
 cp ${HOME}/yocto/meta-iota/templates/bblayers.conf.sample conf/bblayers.conf
 cp ${HOME}/yocto/meta-iota/templates/local.conf.sample conf/local.conf
+
+# wait until there's no bitbake workers running
+while [ -a ${HOME}/yocto/build-$1/bitbake.lock ]
+do
+    sleep 10
+    echo "waiting for other BitBake job to finish..."
+done
+
 MACHINE=$1 bitbake $2
