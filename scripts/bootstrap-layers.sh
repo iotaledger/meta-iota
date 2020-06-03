512475b151dd78cf7d011df8599e499b6c878371
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-06-03 02:28:54 +0000

    add conditionals to bootstrap-layers script

--- scripts/bootstrap-layers.sh
+++ scripts/bootstrap-layers.sh
@@ -10,9 +10,32 @@ cd ${HOME}/yocto
 
 BRANCH=warrior
 
-git clone https://github.com/iotaledger/meta-iota.git
-git clone https://git.yoctoproject.org/git/poky.git -b $BRANCH
-git clone https://github.com/openembedded/meta-openembedded.git -b $BRANCH
-git clone https://git.yoctoproject.org/git/meta-raspberrypi.git -b $BRANCH
-git clone https://github.com/STMicroelectronics/meta-st-stm32mp.git -b $BRANCH
-git clone https://github.com/bernardoaraujor/meta-rock64.git
+if [ ! -d meta-iota ]
+then
+	git clone https://github.com/iotaledger/meta-iota.git
+fi
+
+if [ ! -d poky ]
+then
+	git clone https://git.yoctoproject.org/git/poky.git -b $BRANCH
+fi
+
+if [ ! -d meta-openembedded ]
+then
+	git clone https://github.com/openembedded/meta-openembedded.git -b $BRANCH
+fi
+
+if [ ! -d meta-raspberrypi ]
+then
+	git clone https://git.yoctoproject.org/git/meta-raspberrypi.git -b $BRANCH
+fi
+
+if [ ! -d meta-st-stm32mp ]
+then
+	git clone https://github.com/STMicroelectronics/meta-st-stm32mp.git -b $BRANCH
+fi
+
+if [ ! -d meta-rock64 ]
+then
+	git clone https://github.com/bernardoaraujor/meta-rock64.git
+fi
